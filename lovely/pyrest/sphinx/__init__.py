7deaa02aa7fa29ab088817b55defc505f12bc5f4
Philipp Bogensberger <bogensberger@lovelysystems.com>
2014-01-14 15:06:19 +0100

    added sphinx option for pyramid_config path. With this option set url prefixes are rendered correctly and it's not necessary to specify a module in service directives

--- lovely/pyrest/sphinx/__init__.py
+++ lovely/pyrest/sphinx/__init__.py
@@ -3,3 +3,4 @@ from lovely.pyrest.sphinx.service import ServiceDirective
 
 def setup(sphinx):
     sphinx.add_directive('service', ServiceDirective)
+    sphinx.add_config_value('pyramid_conf', None, 'env')
